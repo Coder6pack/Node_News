b1b279149609790110ea15fbe7666397e57636a7
Coder6pack <105229432+Coder6pack@users.noreply.github.com>
2023-06-08 05:39:02 +0700

    dev1 1st commit

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 import configViewEngine from "./configs/viewEngine";
 import initWebRouter from "./routers/web"
+//import pool from "./configs/connectDB"
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 configViewEngine(app);
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", ".ejs");
